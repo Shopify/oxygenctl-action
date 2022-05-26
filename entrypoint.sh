789cc2617211c8f218e50188910576c6651646af
Uri Gorelik <uri.gorelik@shopify.com>
2022-05-26 15:34:43 -0400

    Require shop and storefront IDs, and a build command
    oxygenctl now generates the deployment ID and executes a build command
    with the OXYGEN_ASSET_BASE_URL environment variable.
    
    By requiring the shop and storefront IDs oxygenctl can now generate the
    asset base URL. This change removes the requirement for oxygen targets
    to hardcode the asset base URL in their deploy hook.

--- entrypoint.sh
+++ entrypoint.sh
@@ -11,13 +11,15 @@ export OXYGEN_DEPLOYMENT_TOKEN="$INPUT_OXYGEN_DEPLOYMENT_TOKEN"
 export OXYGEN_COMMIT_MESSAGE="$INPUT_COMMIT_MESSAGE"
 export OXYGEN_COMMIT_TIMESTAMP="$INPUT_COMMIT_TIMESTAMP"
 export OXYGEN_WORKFLOW_ID="$INPUT_WORKFLOW_RUN_ID"
+export OXYGEN_SHOP_ID="$INPUT_SHOP_ID"
+export OXYGEN_STOREFRONT_ID="$INPUT_STOREFRONT_ID"
+export OXYGEN_BUILD_COMMAND="$INPUT_BUILD_COMMAND"
 
 oxygenctl --version
 
 # Temporarily ignoring that successful deploys result in 502s, that's why we || true
 preview_url="$(
   oxygenctl deploy \
-    --id "$INPUT_DEPLOYMENT_ID" \
     --assets-dir "$INPUT_OXYGEN_CLIENT_DIR" \
     --worker-dir "$INPUT_OXYGEN_WORKER_DIR" \
     --dms-address "$INPUT_OXYGEN_DMS_ADDRESS"
