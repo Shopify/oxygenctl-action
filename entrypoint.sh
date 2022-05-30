3a0943d9ee84746cbb0d5e7f1e3511c094dc6250
Uri Gorelik <uri.gorelik@shopify.com>
2022-05-30 14:01:00 -0400

    Use $oxygenctl_bin var
    Cannot run oxygenctl as ./oxygenctl since entrypoint.sh is run from the
    default working directory so oxygenctl needs the correct path.
    
    /action-directory/entrypoint.sh
    /action-directory/oxygenctl
    /working-dir/
        yarn install
        entrypoint.sh tries to run "./oxygenctl" -> no oxygenctl command in /working-dir/oxygenctl
    
    This commit uses `dirname` to extract entrypoint.sh's directory and
    use it as a prefix for calling oxygenctl while maintaining the working
    directory so the build command can run properly.

--- entrypoint.sh
+++ entrypoint.sh
@@ -14,13 +14,13 @@ export OXYGEN_SHOP_ID="$INPUT_SHOP_ID"
 export OXYGEN_STOREFRONT_ID="$INPUT_STOREFRONT_ID"
 export OXYGEN_BUILD_COMMAND="$INPUT_BUILD_COMMAND"
 
-curdir = $(dirname $0)
+oxygenctl_bin="$(dirname $0)/oxygenctl"
 
-$curdir/oxygenctl --version
+$oxygenctl_bin --version
 
 # Temporarily ignoring that successful deploys result in 502s, that's why we || true
 preview_url="$(
-  $curdir/oxygenctl deploy \
+  $oxygenctl_bin deploy \
     --assets-dir "$INPUT_OXYGEN_CLIENT_DIR" \
     --worker-dir "$INPUT_OXYGEN_WORKER_DIR" \
     --dms-address "$INPUT_OXYGEN_DMS_ADDRESS"
