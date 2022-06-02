520d6bd0d643f3324367a98dc03a45d4ef9401c9
Lance Lafontaine <lance.lafontaine@shopify.com>
2022-06-02 08:32:52 -0400

    Uses new unique-per-rerun workflow_id in oxygenctl-action@v3

--- entrypoint.sh
+++ entrypoint.sh
@@ -9,16 +9,17 @@ set -eu
 export OXYGEN_DEPLOYMENT_TOKEN="$INPUT_OXYGEN_DEPLOYMENT_TOKEN"
 export OXYGEN_COMMIT_MESSAGE="$INPUT_COMMIT_MESSAGE"
 export OXYGEN_COMMIT_TIMESTAMP="$INPUT_COMMIT_TIMESTAMP"
-export OXYGEN_WORKFLOW_ID="$INPUT_WORKFLOW_RUN_ID"
 export OXYGEN_SHOP_ID="$INPUT_SHOP_ID"
 export OXYGEN_STOREFRONT_ID="$INPUT_STOREFRONT_ID"
 export OXYGEN_BUILD_COMMAND="$INPUT_BUILD_COMMAND"
+# We're purposefully ignoring the workflow ID input. It is unnecessary since it is already available as an env var.
+# Since removing an input is a breaking change, it will be removed in the next version (v4)
+export OXYGEN_WORKFLOW_ID="${GITHUB_RUN_ID}_${GITHUB_RUN_ATTEMPT}"
 
 oxygenctl_bin="$(dirname $0)/oxygenctl"
 
 $oxygenctl_bin --version
 
-# Temporarily ignoring that successful deploys result in 502s, that's why we || true
 preview_url="$(
   $oxygenctl_bin deploy \
     --assets-dir "$INPUT_OXYGEN_CLIENT_DIR" \
@@ -26,5 +27,4 @@ preview_url="$(
     --dms-address "$INPUT_OXYGEN_DMS_ADDRESS"
 )"
 
-# Hardcoded storefont name for now
 echo "::set-output name=url::$preview_url"
